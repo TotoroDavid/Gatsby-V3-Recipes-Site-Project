0df38083b9e7344d0d881ccc915143044603e850
TotoroDavid <davidtoro65@hotmail.com>
2021-04-14 20:35:25 -0500

    setting the sources with graphql

--- src/exaples/fechData.js
+++ src/exaples/fechData.js
@@ -1,39 +1,42 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 
-const ComponentName = () => {
-    const data = useStaticQuery(graphql`
-    {
-        site {
-        siteMetadata {
-            author
-            description
-            simpleData
-            title
-            complexData {
-                age
-                name
-            }
-            person {
+const getData = graphql`
+    query FirstQuery  {
+    site {
+        info : siteMetadata {
+        author
+        description
+        simpleData
+        title
+        complexData {
             age
             name
-            }
+        }
+        person {
+            age
+            name
         }
         }
     }
-    `)
-    return <div>
-        <h2>{data.site.siteMetadata.person.name}</h2>
+    }
+`
+
+const FechData = () => {
+
+    const {
+        site:{
+            info:{title},
+        },
+    } = useStaticQuery(getData)
+
+
+    return (
         <div>
-            {data.site.siteMetadata.complexData.map((item,index)=>{
-                return (
-                    <p key={index}>
-                        {item.name}:{item.age}
-                    </p>
-                )
-            })}
+            {/* <h2>Name : {data.site.siteMetadata.person.name}</h2> */}
+            <h2>site title is : {title}</h2>
         </div>
-    </div>
+    )
 }
 
-export default ComponentName
+export default FechData
