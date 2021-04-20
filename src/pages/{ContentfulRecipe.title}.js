7e0d65d9bd898a2717fd88a57311166f743895e1
TotoroDavid <davidtoro65@hotmail.com>
2021-04-20 18:44:17 -0500

    recipe templete query

---
+++ src/pages/{ContentfulRecipe.title}.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+const RecipeTemplate = (props) => {
+    return (
+        <div>
+            <h2>{props.params.title}</h2>
+        </div>
+    )
+}
+
+export const query = graphql `
+    query getSingleRecipe($title: String) {
+    contentfulRecipe(title: {eq: $title}) {
+        title
+        cookTime
+        content {
+        ingredients
+        instructions
+        tags
+        tools
+        }
+        description {
+        description
+        }
+        prepTime
+        servings
+        image {
+        gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+        }
+    }
+}
+`
+
+export default RecipeTemplate
