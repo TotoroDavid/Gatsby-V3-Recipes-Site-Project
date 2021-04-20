d31783f0e9e9ce4de4c1a869390485aa744ee2b9
TotoroDavid <davidtoro65@hotmail.com>
2021-04-20 11:46:50 -0500

    added the images and all details of the recipes

--- src/components/AllRecipes.js
+++ src/components/AllRecipes.js
@@ -1,12 +1,37 @@
 import React from 'react'
 import TagsList from './TagsList'
+import RecipesList from './recipesList'
+import { graphql, useStaticQuery } from 'gatsby'
+
+const query = graphql`
+    {
+        allContentfulRecipe(sort: {fields: title, order: ASC}) {
+        nodes {
+            id
+            title
+            cookTime
+            prepTime
+            content {
+            tags
+            }
+            image {
+            gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+            }
+        }
+        }
+    }
+    `
 
 const AllRecipes = () => {
+    const data = useStaticQuery(query)
+    const recipes = data.allContentfulRecipe.nodes
+    console.log(recipes)
     return (
-        <div>
-            <h4>all recipes</h4>
+        <section className="recipes-container">
             
-        </div>
+            <TagsList recipes={recipes} />
+            <RecipesList recipes={recipes} />
+        </section>
     )
 }
 
